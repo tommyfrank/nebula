0cacbd2b1316ec96f54c630a63f3a1161273192b
tommyfrank <1tommyfrank@gmail.com>
2016-05-20 09:35:04 -0400

    initialized Art.java
    created drawGraphics method

---
+++ src/system/Art.java
@@ -0,0 +1,12 @@
+package system;
+
+import java.awt.Graphics2D;
+import java.awt.image.ImageObserver;
+
+public class Art {
+    
+    public static void drawGraphics(Graphics2D graphics2D, ImageObserver imageObserver) {
+        
+    }
+    
+}
